67a52069fbe66170156f6957cd77476f0a7001e0
Prithviraj Chavan <prithvirajchavan@airasia.com>
2023-06-04 14:12:55 +0530

    create_react_app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hellow world</h1>s
+    </div>
+  );
+}
+
+export default App;
